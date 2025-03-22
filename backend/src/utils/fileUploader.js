8d81110eb922999563a43f6f6984dfdbe61b8d23
Subrata Mondal <subratamondal@tutanota.com>
2025-03-22 15:57:11 +0530

    solve file import error

--- backend/src/utils/fileUploader.js
+++ backend/src/utils/fileUploader.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import addToDelete from "../../storage/log/addToDelete";
+import addToDelete from "../../storage/log/addToDelete.js";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
