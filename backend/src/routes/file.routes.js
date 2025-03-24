ab376b7ff8a779ccb483245fcb57ef97d7525a0f
Subrata Mondal <subratamondal@tutanota.com>
2025-03-24 20:00:00 +0530

    add req abort layer for file upload

--- backend/src/routes/file.routes.js
+++ backend/src/routes/file.routes.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { Router } from "express";
 import upload from "../middlewares/multer.middleware.js";
 import {
@@ -8,6 +9,18 @@ import {
 } from "../controllers/file.controllers.js";
 import verifyUser from "../middlewares/verifyUser.middleware.js";
 import selectFolder from "../middlewares/selectFolder.middleware.js";
+import addToDelete from "../../storage/log/addToDelete.js";
+
+const reqAbort = (req, res, next) => {
+  req.on("aborted", () => {
+    const filePath = req.file?.path;
+    if (filePath && fs.existsSync(filePath)) {
+      addToDelete("local", filePath, new Error("Request Aborted"));
+    }
+  });
+
+  next();
+};
 
 const router = Router();
 
@@ -16,12 +29,19 @@ router.post(
   verifyUser,
   selectFolder,
   upload.single("file"),
+  reqAbort,
   createFileDoc
 );
 router
   .route("/:id")
   .get(getFile)
-  .patch(verifyUser, selectFolder, upload.single("file"), updateFileDoc)
+  .patch(
+    verifyUser,
+    selectFolder,
+    upload.single("file"),
+    reqAbort,
+    updateFileDoc
+  )
   .delete(verifyUser, deleteFileDoc);
 
 export default router;
