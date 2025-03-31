c7a228710e3b685d9f16e692055d8b9dc98e52df
Subrata Mondal <subratamondal@tutanota.com>
2025-03-31 09:29:33 +0530

    complete cart controller

--- backend/src/controllers/cart.controllers.js
+++ backend/src/controllers/cart.controllers.js
@@ -1,61 +1,55 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
 import Cart from "../models/cart.model.js";
-
-const createCart = asyncHandler(async (req, res) => {
-  const { product } = req.body;
-
-  if (!product || Object.entries(product).length === 0)
-    throw new ApiError(400, "Product is required");
-
-  if (!product.productId || !product.quantity)
-    throw new ApiError(400, "Product is required");
-
-  const cart = await Cart.create({
-    userId: req.user._id,
-    products: [product],
-  });
-
-  if (!cart) throw new ApiError(500, "Failed to create cart");
-
-  res.status(201).json(new ApiResponse(201, "Cart created successfully", cart));
-});
 
 const addProductToCart = asyncHandler(async (req, res) => {
   const { product } = req.body;
-  const cartId = req.params.id;
 
   if (!product || Object.entries(product).length === 0)
     throw new ApiError(400, "Product is required");
 
-  if (!product.productId || !product.quantity)
-    throw new ApiError(400, "Product is required");
+  if (!product.productId) throw new ApiError(400, "ProductId is required");
 
-  const cart = await Cart.findById(cartId);
-  if (!cart) throw new ApiError(404, "Cart not found");
+  let cart = await Cart.findOne({ userId: req.user._id, isOrdered: false });
 
-  if (req.user._id !== cart.userId) throw new ApiError(403, "Unauthorized");
+  if (!cart) {
+    cart = new Cart({
+      userId: req.user._id,
+    });
+  }
 
   const availableProductIndex = cart.products.findIndex(
-    (p) => p.productId === product.productId
+    (p) => p.productId.toString() === product.productId.toString()
   );
 
   if (availableProductIndex !== -1) {
-    cart.products[availableProductIndex].quantity = product.quantity;
+    const existingProduct = cart.products[availableProductIndex];
+
+    if (typeof product.quantity === "number") {
+      if (existingProduct.quantity === product.quantity) {
+        throw new ApiError(400, "Quantity must be more or less than current");
+      } else if (product.quantity === 0) {
+        cart.products.splice(availableProductIndex, 1);
+      } else {
+        existingProduct.quantity = product.quantity;
+      }
+    }
   } else {
+    if (product.quantity === 0)
+      throw new ApiError(400, "Quantity must be more than 0");
     cart.products.push(product);
   }
 
   await cart.save();
 
-  res.json(new ApiResponse(200, "Product added to cart successfully", cart));
+  res.json(new ApiResponse(200, "Cart updated successfully", cart));
 });
 
 const getCart = asyncHandler(async (req, res) => {
-  const cartId = req.params.id;
-
   const [cart] = await Cart.aggregate([
-    { $match: { _id: cartId, userId: req.user._id, isOrdered: false } },
-    { $unwind: "$products" },
+    { $match: { userId: req.user._id, isOrdered: false } },
+    { $unwind: { path: "$products", preserveNullAndEmptyArrays: true } },
     {
       $lookup: {
         from: "products",
@@ -65,11 +59,11 @@ const getCart = asyncHandler(async (req, res) => {
       },
     },
     {
-      $unwind: "$product",
+      $unwind: { path: "$product", preserveNullAndEmptyArrays: true },
     },
     {
       $project: {
-        _id: 0,
+        _id: "$_id",
         product: "$product",
         quantity: "$products.quantity",
       },
@@ -80,6 +74,17 @@ const getCart = asyncHandler(async (req, res) => {
         products: { $push: { product: "$product", quantity: "$quantity" } },
       },
     },
+    {
+      $addFields: {
+        products: {
+          $cond: {
+            if: { $eq: ["$products", [{}]] },
+            then: [],
+            else: "$products",
+          },
+        },
+      },
+    },
   ]);
 
   if (!cart) throw new ApiError(404, "Cart not found");
@@ -87,4 +92,4 @@ const getCart = asyncHandler(async (req, res) => {
   res.json(new ApiResponse(200, "Cart fetched successfully", cart));
 });
 
-export { createCart, addProductToCart, getCart };
+export { addProductToCart, getCart };
--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,16 +1,11 @@
 import { Router } from "express";
 import verifyUser from "../middlewares/verifyUser.middleware.js";
-import {
-  createCart,
-  getCart,
-  addProductToCart,
-} from "../controllers/cart.controllers.js";
+import { getCart, addProductToCart } from "../controllers/cart.controllers.js";
 
 const router = Router();
 
 router.use(verifyUser);
 
-router.post("/", createCart);
-router.route("/:id").get(getCart).put(addProductToCart);
+router.route("/").get(getCart).put(addProductToCart);
 
 export default router;
