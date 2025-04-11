4dc57e509e426fe08b8e65cab3af0e25ed398b10
Subrata Mondal <subratamondal@tutanota.com>
2025-04-11 23:46:08 +0530

    add health checker and update the document

--- backend/src/controllers/test.controllers.js
+++ backend/src/controllers/test.controllers.js
@@ -1,12 +1,6 @@
-import { isValidObjectId } from "mongoose";
-import ApiError from "../utils/apiError.js";
+import mongoose from "mongoose";
 import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import axios from "axios";
-import nodemailer from "nodemailer";
-import File from "../models/file.model.js";
-import fs from "fs";
-import { v2 as cloudinary } from "cloudinary";
 
 const sendSuccess = asyncHandler((req, res) => {
   res.json(new ApiResponse());
@@ -16,117 +10,16 @@ const throwError = asyncHandler(async (req, res) => {
   throw new Error("This is an error");
 });
 
-const validObjectId = asyncHandler(async (req, res) => {
-  res.json(
-    new ApiResponse(200, "Success", { isValid: isValidObjectId(req.user._id) })
-  );
+const healthCheck = asyncHandler(async (req, res) => {
+  const db = await (await mongoose.connection.db.stats()).db;
+  const data = {
+    uptime: process.uptime(),
+    db,
+    environment: process.env.NODE_ENV,
+    timeStamp: new Date(),
+  };
+
+  res.json(new ApiResponse(200, "Success", data));
 });
 
-const imageSend = asyncHandler(async (req, res) => {
-  // const file = await File.findById(req.params.id);
-
-  // if (!file) throw new ApiError(404, "File not found");
-
-  // if (req.headers["if-none-match"] === file._id.toString()) {
-  //   return res.status(304).end(); // Not Modified
-  // }
-
-  const secureUrl = cloudinary.url(req.query.id, {
-    secure: true,
-    resource_type: "raw",
-  });
-
-  const response = await axios({
-    method: "GET",
-    url: secureUrl,
-    responseType: "stream",
-  });
-
-  const isDownload = req.query.download;
-
-  res.setHeader(
-    "Content-Disposition",
-    `${isDownload !== undefined ? "attachment" : "inline"}; filename="${
-      file?.name || "file"
-    }.pdf"`
-  );
-  res.setHeader("Content-Type", response.headers["content-type"]);
-  res.setHeader("Content-Length", response.headers["content-length"]);
-  res.setHeader("Cache-Control", "public, max-age=31536000");
-  // res.setHeader("ETag", file._id.toString());
-
-  response.data.pipe(res);
-});
-
-const uploadPdf = asyncHandler(async (req, res) => {
-  const file = req.file.path;
-
-  if (!file) throw new ApiError(500, "File Upload failed");
-
-  const upload = await cloudinary.uploader.upload(file, {
-    resource_type: "raw",
-    folder: "techkart/invoices",
-  });
-  console.log(
-    `pdf uploaded successfully ${(upload.secure_url, upload.public_id)}`
-  );
-  res.json(new ApiResponse(200, "Success", upload));
-});
-
-const reqAbort = asyncHandler(async (req, res) => {
-  setTimeout(() => {
-    res.json(new ApiResponse(200, "Success"));
-  }, 5000);
-});
-
-const sendMail = asyncHandler(async (req, res) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: `"Maddison Foo Koch 👻"`, // sender address
-    to: process.env.SMTP_RECEIVER_EMAILS, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  res.json(new ApiResponse(200, "Success", info));
-});
-
-const deleteRequest = asyncHandler(async (req, res) => {
-  const { status } = req.params;
-  let response;
-  const baseUrl = `${req.protocol}://${req.get("host")}`;
-
-  try {
-    if (status === "success") {
-      response = await axios.get(`${baseUrl}/api/v1/test/success`);
-    } else {
-      response = await axios.get(`${baseUrl}/api/v1/test/error`);
-    }
-  } catch (error) {
-    throw new ApiError(500, "Something went wrong");
-  }
-
-  res.json(new ApiResponse(200, "Success", response?.data));
-});
-
-export {
-  sendSuccess,
-  throwError,
-  validObjectId,
-  imageSend,
-  uploadPdf,
-  sendMail,
-  reqAbort,
-  deleteRequest,
-};
+export { sendSuccess, throwError, healthCheck };
--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -1,24 +1,14 @@
 import { Router } from "express";
 import {
-  imageSend,
   sendSuccess,
   throwError,
-  validObjectId,
-  reqAbort,
-  sendMail,
-  deleteRequest,
+  healthCheck,
 } from "../controllers/test.controllers.js";
-import verifyUser from "../middlewares/verifyUser.middleware.js";
-import asyncHandler from "../utils/asyncHandler.js";
-import ApiResponse from "../utils/apiResponse.js";
 
 const router = Router();
 
 router.get("/success", sendSuccess);
 router.get("/error", throwError);
-router.get("/validObjectId", verifyUser, validObjectId);
-router.get("/abort", reqAbort);
-router.get("/sendMail", sendMail);
-router.post("/request/:status", deleteRequest);
+router.get("/health", healthCheck);
 
 export default router;
