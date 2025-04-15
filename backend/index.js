73d55559cf1010a2bbb798e729481086e1b5f87a
Subrata Mondal <subratamondal@tutanota.com>
2025-04-15 09:10:42 +0530

    added and connected redis server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 import "./src/config/dotenvConfig.js";
 import app from "./src/app.js";
-import connectDB from "./src/db.js";
+import connectMongoDB from "./src/db/mongodb.db.js";
+import redisClient from "./src/db/redis.db.js";
 
 const port = process.env.PORT || 8000;
 
-connectDB().then(() => {
-  const server = app.listen(port, () => {
-    console.log("Server is Running on:", server.address().port);
-  });
-});
+(async () => {
+  try {
+    await connectMongoDB();
+    await redisClient.connect();
+    app.listen(port, () => console.log(`Server is Running on: ${port}`));
+  } catch (error) {
+    console.error("Initialization error: " + error);
+  }
+})();
