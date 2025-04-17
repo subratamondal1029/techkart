cb89e480c6b618bdebc18a2407cdaba2ae4d999f
Subrata Mondal <subratamondal@tutanota.com>
2025-04-17 18:47:38 +0530

    add lazy loading for component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,17 +1,19 @@
-import Input from "./formComp/Input";
-import TextArea from "./formComp/TextArea";
-import Button from "./Button";
-import ButtonLoading from "./ButtonLoading";
-import ProtectedRoute from "../pages/ProtectedRoute";
-import Header from "./Header";
-import Footer from "./Footer";
-import Logo from "./Logo";
-import ProductCard from "./ProductCard";
-import MainLoader from "./MainLoader";
-import AccountInfoCard from "./AccountInfoCard";
-import OrderStatus from "./OrderStatus";
-import CartPop from "./CartPop";
-import AccessDenied from "./AccessDenied";
+import { lazy } from "react";
+
+const Input = lazy(() => import("./formComp/Input"));
+const TextArea = lazy(() => import("./formComp/TextArea"));
+const Button = lazy(() => import("./Button"));
+const ButtonLoading = lazy(() => import("./ButtonLoading"));
+const ProtectedRoute = lazy(() => import("../pages/ProtectedRoute"));
+const Header = lazy(() => import("./Header"));
+const Footer = lazy(() => import("./Footer"));
+const Logo = lazy(() => import("./Logo"));
+const ProductCard = lazy(() => import("./ProductCard"));
+const MainLoader = lazy(() => import("./MainLoader"));
+const AccountInfoCard = lazy(() => import("./AccountInfoCard"));
+const OrderStatus = lazy(() => import("./OrderStatus"));
+const CartPop = lazy(() => import("./CartPop"));
+const AccessDenied = lazy(() => import("./AccessDenied"));
 
 export {
   Input,
@@ -27,5 +29,5 @@ export {
   AccountInfoCard,
   OrderStatus,
   CartPop,
-  AccessDenied
+  AccessDenied,
 };
