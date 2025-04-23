348b91e1efabeb91fd76fdc6dc3c12f50d42631c
Subrata Mondal <subratamondal@tutanota.com>
2025-04-23 15:57:39 +0530

    feat: implement Image component with loading and error handling states

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Image = () => {
-  return <div>Image</div>;
+/**
+ *
+ * @param {React.ImgHTMLAttributes<HTMLImageElement>} props
+ * @returns {React.ReactElement}
+ */
+
+const Image = ({ src, alt = "image", className = "", ...props }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleLoad = () => {
+    setLoaded(true);
+    setError(false);
+  };
+  const handleError = () => {
+    setError(true);
+    setLoaded(false);
+  };
+
+  useEffect(() => {
+    const msg = error
+      ? "Image Loading error"
+      : !loaded
+      ? "Image Loading"
+      : "Image Loaded";
+    console.log(msg);
+  }, [error, loaded]);
+
+  return (
+    <div className={`overflow-hidden ${className}`}>
+      {error ? (
+        <div className="w-full h-full bg-gray-200">Image Loading error</div>
+      ) : !loaded ? (
+        <div className="w-full h-full bg-gray-300 animate-pulse">
+          Image Loading
+        </div>
+      ) : null}
+      <img
+        src={src}
+        onLoad={handleLoad}
+        onError={handleError}
+        className={`transition-opacity duration-500 w-full h-full object-cover`}
+        {...props}
+      />
+    </div>
+  );
 };
 
 export default Image;
--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import testService from "../services/test.service";
+import fileService from "../services/file.service";
 import showToast from "../utils/showToast";
 import { useLoading } from "../hooks";
-import { ButtonLoading, Button } from ".";
+import { ButtonLoading, Button, Image } from ".";
 
 const Test = () => {
   const [run, isLoading, error] = useLoading(async () => {
@@ -15,8 +16,10 @@ const Test = () => {
     <div className="bg-gray-700 text-white h-screen w-full">
       <h1 className="text-3xl ">Test</h1>
       <p>{error}</p>
-      {/* {isLoading ? <ButtonLoading /> : <Button onClick={run}>run</Button>} */}
-      <ButtonLoading />
+      <Image
+        src={fileService.get("67eeb1fcfaf070cbfb48da8c")}
+        className="rounded-xl w-48 h-48"
+      />
     </div>
   );
 };
