6f8cc33a88319c8e4fdf526adb7db115300f7fab
Subrata Mondal <subratamondal@tutanota.com>
2025-04-26 22:42:41 +0530

    write jsDoc for Easy usage in multiple component

--- frontend/src/hooks/useInfiniteScroll.js
+++ frontend/src/hooks/useInfiniteScroll.js
@@ -1,6 +1,20 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import useLoading from "./useLoading";
 
+/**
+ * A custom hook for implementing infinite scrolling using the Intersection Observer API.
+ *
+ * @param {Function} cb - The callback function to fetch data. It should accept the current page number as an argument.
+ * @param {React.RefObject<HTMLElement>} loaderRef - A React ref pointing to the loader element that triggers the next page load when visible.
+ * @param {...any} args - Additional arguments to pass to the callback function.
+ * @returns {[number, boolean, string, Function, Function]} - Returns an array containing:
+ *   - `page` (number): The current page number.
+ *   - `isLoading` (boolean): Whether data is currently being fetched.
+ *   - `error` (string): Error message if the fetch fails.
+ *   - `retry` (Function): A function to retry fetching data in case of an error.
+ *   - `setPage` (Function): A function to manually set the page number.
+ */
+
 const useInfiniteScroll = (cb, loaderRef, ...args) => {
   const [page, setPage] = useState(0);
 
--- frontend/src/hooks/useLoading.js
+++ frontend/src/hooks/useLoading.js
@@ -1,5 +1,15 @@
 import { useState, useCallback } from "react";
 
+/**
+ * A custom hook for show loading status and handle error during api call and asynchronous process.
+ *
+ * @param {Function} asyncFn - The callback function to fetch data.
+ * @returns {[Function, boolean, string]} - Returns an array containing:
+ *   - `run` (function): The async function that send by parameter with a wrapper.
+ *   - `isLoading` (boolean): Whether data is currently being fetched.
+ *   - `error` (string): Error message if the fetch fails.
+ */
+
 const useLoading = (asyncFn) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
