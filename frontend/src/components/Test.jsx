aa69c3cfe4e6584424590806c08a19f1546bf9ec
Subrata Mondal <subratamondal@tutanota.com>
2025-04-26 22:43:21 +0530

    test the infinite scrolling with useInfiniteScroll custom hook

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -5,13 +5,10 @@ import { useLoading } from "../hooks";
 import { ButtonLoading, Button, Image, OrderStatus } from ".";
 import { useOptimistic, useState, startTransition } from "react";
 import delay from "../utils/delay";
-
-const sampleStatus = {
-  isShipped: true,
-  isDelivered: true,
-  isCancelled: true,
-  isRefund: true,
-};
+import { LoaderCircle } from "lucide-react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import useInfiniteScroll from "../hooks/useInfiniteScroll";
 
 const Test = () => {
   const [data, setData] = useState([
@@ -60,8 +57,40 @@ const Test = () => {
     });
   };
 
+  const loaderRef = useRef(null);
+  const [products, setProducts] = useState([]);
+  const totalPages = useRef(2);
+  const [filter, setFilter] = useState({
+    sort: "a",
+    sortBy: "price",
+    query: "",
+  });
+
+  const [page, isLoading, error, retry, setPage] = useInfiniteScroll(
+    async (page, filter) => {
+      console.log("filter", filter);
+
+      console.log(`fetching page: ${page} with query ${filter.query}`);
+      await apiCall();
+      console.log(`fetched page: ${page}`);
+      if (page === 1) {
+        setProducts(Array.from({ length: 10 }));
+      } else {
+        setProducts((prev) => [...prev, ...Array.from({ length: 10 })]);
+      }
+    },
+    loaderRef,
+    filter
+  );
+
+  useEffect(() => {
+    if (!page) return;
+    setPage(0);
+    setProducts([]);
+  }, [filter]);
+
   return (
-    <div className="bg-gray-700 text-white w-full h-screen flex  items-center flex-col">
+    <div className="bg-gray-700 text-white w-full min-h-screen flex  items-center flex-col">
       <h1 className="text-3xl ">Test</h1>
       <div className="flex justify-center items-center w-full space-x-5">
         {optimisticData.map((item) => (
@@ -85,8 +114,41 @@ const Test = () => {
           </div>
         ))}
       </div>
-      <div className="mt-10">
-        status: <OrderStatus order={sampleStatus} />
+      <div className="w-full min-h-screen bg-gray-900 flex flex-col justify-start items-center space-y-5 flex-wrap py-2 mt-3">
+        <div className="fixed bottom-10 right-0">
+          <input
+            type="text"
+            defaultValue={filter.query}
+            onBlur={(e) =>
+              setFilter((prev) => {
+                const query = e.target.value?.trim();
+                if (query === prev.query || !query) return prev;
+
+                return { ...prev, query };
+              })
+            }
+            placeholder="Search"
+            className="focus:outline-none text-black p-2"
+          />
+        </div>
+        {products.map((_, index) => (
+          <div
+            className="bg-gray-400 w-32 h-32 rounded flex justify-center items-center text-2xl"
+            key={index}
+          >
+            product {index + 1}
+          </div>
+        ))}
+        {error ? (
+          <>
+            <h1 className="text-2xl text-red-500">{error}</h1>
+            <Button onClick={retry}>retry</Button>
+          </>
+        ) : (
+          (isLoading || page < totalPages.current) && (
+            <LoaderCircle className="animate-spin " size={30} ref={loaderRef} />
+          )
+        )}
       </div>
     </div>
   );
