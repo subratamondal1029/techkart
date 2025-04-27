410a4eedfd85b6da08e4f81eefba82876298d81e
Subrata Mondal <subratamondal@tutanota.com>
2025-04-27 15:17:10 +0530

    feat: enhance order details page with improved data fetching and rendering logic

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,243 +1,211 @@
 import React, { useEffect, useState } from "react";
-import { Button, ButtonLoading, OrderStatus } from "../components";
+import { Button, ButtonLoading, Image, OrderStatus } from "../components";
+import OrderShimmer from "../components/shimmers/Order.shimmer";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import appWriteDb from "../appwrite/DbServise";
-import appWriteStorage from "../appwrite/storageService";
 import { Download } from "lucide-react";
 import { toast } from "react-toastify";
-import { login } from "../store/authSlice";
-import { Query } from "appwrite";
+import { useLoading } from "../hooks";
+import fileService from "../services/file.service";
+import orderService from "../services/order.service";
+import { addOrder } from "../store/order.slice";
 
 export default function Orders() {
-  const orders = useSelector((state) => state.auth.otherData.orders);
-  const cart = useSelector((state) => state.auth.otherData.cart);
-  const { products: allProducts } = useSelector((state) => state.products);
-  const { orderId } = useParams();
+  const { id } = useParams();
+  const existingOrder = useSelector((state) =>
+    state.orders.find((order) => order._id === id)
+  );
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
-  const [order, setOrder] = useState({
-    date: "",
-    id: "",
-    products: [],
-    userId: "",
-    name: "",
-    phone: "",
-    address: "",
-    isShipped: false,
-    isDelivered: false,
+  const [order, setOrder] = useState(null);
+
+  const formateDate = (date) => {
+    date = new Date(date);
+    return `${date.getDate().toString().padStart(2, "0")}-${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}-${date.getFullYear()}`;
+  };
+
+  const calCartTotal = (cart) => {
+    return cart?.products?.reduce(
+      (acc, cur) => acc + cur.product.price * cur.quantity,
+      0
+    );
+  };
+
+  const [fetchOrder, isFetchingOrder, orderError] = useLoading(async () => {
+    const { data: order } = await orderService.getOne(id);
+    dispatch(addOrder(order));
   });
 
-  useEffect(() => {
-    // TODO: fetch order from server or slice
-    if (!orderId) navigate("/account");
-    const getProducts = (cart) => {
-      return cart
-        .map((product) => JSON.parse(product))
-        .map((cartProduct) => {
-          const productDetails = allProducts.find(
-            (product) => product.$id === cartProduct.productId
-          );
-          return {
-            id: productDetails.$id,
-            quantity: cartProduct.quantity,
-            name: productDetails.name,
-            price: productDetails.price,
-            imageSrc: productDetails.image,
-          };
-        });
-    };
+  const [cancelOrder, isCancelling] = useLoading(async () => {
+    const cancelRequest = orderService.cancel(id);
 
-    const order = orders
-      .filter((order) => order.$id === orderId)
-      .map((order) => ({
-        date: order.date,
-        id: order.$id,
-        products: getProducts(order.cart),
-        userId: order.userId,
-        name: order.name,
-        phone: order.phone,
-        address: order.address,
-        isShipped: order.isShipped,
-        isDelivered: order.isDeliverd,
-      }));
-    if (order.length === 0) {
-      navigate("/account");
-    }
-    setOrder(order[0]);
-  }, []);
-
-  const calTotal = () => {
-    let total = 0;
-    order?.products.forEach((product) => {
-      total += Number(product.price) * product.quantity;
+    toast.promise(cancelRequest, {
+      pending: "Cancelling...",
+      success: "Order cancelled successfully",
+      error: "Failed to cancel order",
     });
-    return total.toLocaleString("en-IN");
-  };
 
-  const downloadInvoice = async () => {
+    const { data } = await cancelRequest;
+    dispatch(
+      addOrder({
+        ...order,
+        isCancelled: data?.isCancelled,
+        isRefund: data?.isRefund,
+        updatedAt: data?.updatedAt,
+        statusUpdateDate: data?.statusUpdateDate,
+        invoice: data?.invoice,
+      })
+    );
+  });
+
+  const downloadInvoice = async (order) => {
     try {
-      // TODO: chnage change the url
-      const downloadLink = await appWriteStorage.getDownloadLink(
-        `${order.id}_invoice`
-      );
-      if (downloadLink) {
-        window.open(downloadLink, "_blank");
-      } else throw new Error("Something went wrong");
+      const downloadLink = fileService.get(order.invoice);
+      window.open(downloadLink, "_blank");
     } catch (error) {
       toast.error(error.message);
     }
   };
 
-  const deleteOrder = async () => {
-    // TODO: cancel order not delete and wrap with loader
-    setIsLoading(true);
-    try {
-      const deleteOrder = await appWriteDb.deleteOrder(orderId);
-      if (deleteOrder) {
-        const newOrders = orders.map((order) => {
-          if (order.$id !== orderId) return order.$id;
-        });
-        const res = await appWriteDb.addOrder(newOrders, order.userId);
-        if (res) {
-          const allorders = await appWriteDb.getOrders([
-            Query.equal("userId", order.userId),
-          ]);
-          dispatch(
-            login({
-              otherData: {
-                orders: allorders,
-                cart: [...cart],
-                isCartCreated: true,
-              },
-            })
-          );
-          toast.success("Order deleted successfully");
-          navigate("/account");
-        } else throw new Error("Something went wrong");
-      }
-    } catch (error) {
-      toast.error(error.message);
-      console.error(error.message);
+  useEffect(() => {
+    if (existingOrder) {
+      setOrder(existingOrder);
+    } else {
+      fetchOrder();
     }
-    setIsLoading(false);
-  };
+  }, [existingOrder, id]);
+
+  if (isFetchingOrder) return <OrderShimmer />;
 
   return (
-    <div className="mx-auto my-4 max-w-4xl md:my-6">
-      <div className="overflow-hidden rounded-xl border border-gray-100 shadow">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          {/* Product List */}
-          <div className="px-5 py-6 md:border-r md:border-r-gray-200 md:px-8">
-            <div className="flow-root">
-              <ul className="-my-7 divide-y divide-gray-200">
-                {order?.products.map((product) => (
-                  <li
-                    key={product.id}
-                    className="flex items-stretch justify-between space-x-5 py-7"
-                  >
-                    <div className="flex flex-1 items-stretch">
-                      <Link
-                        to={`/product/${product.id}`}
-                        className="flex-shrink-0"
-                      >
-                        <img
-                          className="h-20 w-20 rounded-lg border border-gray-200 object-contain"
-                          src={product.imageSrc}
-                          alt={product.name}
-                        />
-                      </Link>
+    order && (
+      <div className="mx-auto my-4 max-w-4xl md:my-6">
+        <div className="overflow-hidden rounded-xl border border-gray-100 shadow">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            {/* Product List */}
+            <div className="px-5 py-6 md:border-r md:border-r-gray-200 md:px-8">
+              <div className="flex flex-col justify-between w-full h-full">
+                <ul className="-my-7 divide-y divide-gray-200">
+                  {order.cart?.products.map(({ product, quantity }) => (
+                    <li
+                      key={product._id}
+                      className="flex items-stretch justify-between space-x-5 py-7"
+                    >
+                      <div className="flex flex-1 items-stretch">
+                        <Link
+                          to={`/product/${product._id}`}
+                          className="flex-shrink-0"
+                        >
+                          <Image
+                            className="h-20 w-20 rounded-lg border border-gray-200 object-contain"
+                            src={fileService.get(product.image)}
+                            alt={product.name}
+                          />
+                        </Link>
 
-                      <div className="ml-5 flex flex-col justify-between">
-                        <div className="flex-1">
-                          <p className="text-sm font-bold text-gray-900">
-                            {product.name}
+                        <div className="ml-5 flex flex-col justify-between">
+                          <div className="flex-1">
+                            <p className="text-sm font-bold text-gray-900">
+                              {product.name}
+                            </p>
+                          </div>
+                          <p className="mt-4 text-sm font-medium text-gray-500">
+                            x {quantity}
                           </p>
                         </div>
-                        <p className="mt-4 text-sm font-medium text-gray-500">
-                          x {product.quantity}
+                      </div>
+                      <div className="ml-auto flex flex-col items-end justify-between">
+                        <p className="text-right text-sm font-bold text-gray-900">
+                          ₹ {product.price}
                         </p>
                       </div>
-                    </div>
-                    <div className="ml-auto flex flex-col items-end justify-between">
-                      <p className="text-right text-sm font-bold text-gray-900">
-                        ₹ {product.price}
+                    </li>
+                  ))}
+                </ul>
+                <div>
+                  <hr className="mt-6 border-gray-200" />
+                  <ul className="mt-6 space-y-3">
+                    <li className="flex items-center justify-between">
+                      <p className="text-sm font-medium">Sub total</p>
+                      <p className="text-sm font-medium">
+                        ₹ {calCartTotal(order.cart)}
                       </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-              <hr className="mt-6 border-gray-200" />
-              <ul className="mt-6 space-y-3">
-                <li className="flex items-center justify-between">
-                  <p className="text-sm font-medium">Sub total</p>
-                  <p className="text-sm font-medium">₹ {calTotal()}</p>
-                </li>
-                <li className="flex items-center justify-between">
-                  <p className="text-sm font-medium ">Total</p>
-                  <p className="text-sm font-bold ">₹ {calTotal()}</p>
-                </li>
-              </ul>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <p className="text-sm font-medium ">Total</p>
+                      <p className="text-sm font-bold ">
+                        ₹ {order.totalAmount}
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-          {/* Contact Info */}
-          <div className="px-5 py-6 md:px-8">
-            <div className="flow-root">
-              <div className="-my-6 divide-y divide-gray-200">
-                <div className="py-6">
-                  <h2 className="text-base font-bold text-black">
-                    Shipping Information
-                  </h2>
-                  <p className="fontmedium mt-3 text-xs text-gray-700">
-                    Order Number: #{order?.id}
-                  </p>
-                  <p className="text-xs font-medium text-gray-700">
-                    Date: {order?.date}
-                  </p>
-                  {order?.isDelivered && (
-                    <Button
-                      type="button"
-                      classname="mt-3 flex justify-center items-center"
-                      onClick={downloadInvoice}
-                    >
-                      <Download className="mr-2 h-4 w-4" size={20} /> Download
-                      Invoice
-                    </Button>
-                  )}
-                </div>
-                <div className="py-6">
-                  <h2 className="mb-2 text-base font-bold text-black">
-                    Contact Information
-                  </h2>
-                  <p className="mt-3 text-xs font-medium text-gray-700">
-                    {order?.name}
-                  </p>
-                  <p className="text-xs font-medium text-gray-700">
-                    {order?.address}
-                  </p>
-                  <p className="text-xs font-medium text-gray-700">
-                    {order?.phone}
-                  </p>
-                </div>
-                <div className="py-6">
-                  <h2 className="text-base font-bold text-black">Status</h2>
-                  <div className="flex items-center justify-between">
-                    <OrderStatus order={order} />
-                    {order.isDelivered || (
+            {/* Contact Info */}
+            <div className="px-5 py-6 md:px-8">
+              <div className="flow-root">
+                <div className="-my-6 divide-y divide-gray-200">
+                  <div className="py-6">
+                    <h2 className="text-base font-bold text-black">
+                      Shipping Information
+                    </h2>
+                    <p className="fontmedium mt-3 text-xs text-gray-700">
+                      Order Number: #{order._id}
+                    </p>
+                    <p className="text-xs font-medium text-gray-700">
+                      {formateDate(order.orderDate)}
+                    </p>
+                    {order.isDelivered && !order.isCancelled && (
                       <Button
                         type="button"
-                        classname="flex justify-center items-center h-9 disabled:cursor-not-allowed disabled:bg-black/50"
-                        disabled={isLoading}
-                        onClick={deleteOrder}
+                        classname="mt-3 flex justify-center items-center"
+                        onClick={() => downloadInvoice(order)}
                       >
-                        {isLoading ? (
-                          <ButtonLoading fillColor="fill-black" />
-                        ) : (
-                          "Cancel"
-                        )}
+                        <Download className="mr-2 h-4 w-4" size={20} /> Download
+                        Invoice
                       </Button>
                     )}
+                  </div>
+                  <div className="py-6">
+                    <h2 className="mb-2 text-base font-bold text-black">
+                      Contact Information
+                    </h2>
+                    <p className="mt-3 text-xs font-medium text-gray-700">
+                      {order.customerName}
+                    </p>
+                    <p className="text-xs font-medium text-gray-700">
+                      {order.address}
+                    </p>
+                    <p className="text-xs font-medium text-gray-700">
+                      {order.customerPhone}
+                    </p>
+                  </div>
+                  <div className="py-6">
+                    <h2 className="text-base font-bold text-black">Status</h2>
+                    <div className="flex items-center justify-between">
+                      <OrderStatus order={order} />
+                      <p className="text-gray-500">
+                        Update on: {formateDate(order.statusUpdateDate)}
+                      </p>
+                      {!order.isShipped && (
+                        <Button
+                          type="button"
+                          classname="flex justify-center items-center h-9"
+                          disabled={isCancelling}
+                          onClick={cancelOrder}
+                        >
+                          {isCancelling ? (
+                            <ButtonLoading fillColor="fill-black" />
+                          ) : (
+                            "Cancel"
+                          )}
+                        </Button>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -245,6 +213,6 @@ export default function Orders() {
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
