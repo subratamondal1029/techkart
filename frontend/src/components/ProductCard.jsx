75d8a4badd370c068c74101a42c42b8d660087f0
Subrata Mondal <subratamondal@tutanota.com>
2025-05-03 20:06:27 +0530

    feat: refactor ProductCard component for improved navigation and add ProductCardShimmer for loading state

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Button } from "./index";
+import { Button, Image } from "./index";
 import fileService from "../services/file.service";
-import { Link } from "react-router-dom";
 import { Eye } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,30 +8,34 @@ export default function ProductCard({ product }) {
   const { _id, image, name, price } = product;
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(`/product/${_id}`);
+  };
+
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md p-5 flex flex-col justify-between ">
-      <Link to={`/product/${_id}`}>
-        {/* TODO: add simmer Effect */}
-        <img
-          className="rounded-lg"
-          src={fileService.get(image)}
-          alt="product image"
-        />
-      </Link>
+    <div
+      className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md p-5 flex flex-col justify-between cursor-pointer"
+      onClick={handleClick}
+    >
+      <Image
+        className="rounded-lg"
+        src={fileService.get(image)}
+        alt="product image"
+      />
+
       <div className="mt-5">
-        <Link to={`/product/${_id}`}>
-          <h5 className="text-xl font-semibold tracking-tight text-gray-800">
-            {name}
-          </h5>
-        </Link>
+        <h5 className="text-xl font-semibold tracking-tight text-gray-800">
+          {name}
+        </h5>
+
         <div className="flex items-center justify-between mt-5">
-          <span className="text-2xl font-bold text-gray-900">
+          <span
+            className="text-2xl font-bold text-gray-900 cursor-default"
+            onClick={(e) => e.stopPropagation()}
+          >
             ₹ {price?.toLocaleString()}
           </span>
-          <Button
-            onClick={() => navigate(`/product/${_id}`)}
-            classname="text-white rounded-lg text-sm px-5 py-2.5 text-center w-32 flex justify-between items-center"
-          >
+          <Button classname="text-white rounded-lg text-sm px-5 py-2.5 text-center w-32 flex justify-between items-center">
             <Eye />
             View
           </Button>
