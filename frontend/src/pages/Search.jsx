a663cfe99beed7c92a895697cc347cd724e58514
Subrata Mondal <subratamondal@tutanota.com>
2025-05-03 21:11:20 +0530

    feat: refactor Search component to implement infinite scroll and improve product fetching logic

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,55 +1,135 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { ProductCard } from "../components";
-import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { LoadingError, ProductCard } from "../components";
+import ProductCardShimmer from "../components/shimmers/ProductCard.shimmer";
+import useInfiniteScroll from "../hooks/useInfiniteScroll";
+import productService from "../services/product.service";
+import { addQuery } from "../store/search.slice";
 
 const Search = () => {
-  const navigate = useNavigate();
-  let { searchValue } = useParams();
-  if (searchValue === "all") searchValue = "";
-  const allProducts = useSelector((state) => state.products.products);
-  const [searchProducts, setSearchProducts] = useState([]);
-  const filterProducts = useCallback(() => {
-    // TODO: fetch the filtered products from server
-    const products = allProducts.filter((product) => {
-      if (
-        product.name.toLowerCase().includes(searchValue) ||
-        product.category.toLowerCase().includes(searchValue) ||
-        product.company.toLowerCase().includes(searchValue) ||
-        product.description.toLowerCase().includes(searchValue) ||
-        product.tags.some((tag) => tag.toLowerCase().includes(searchValue))
-      ) {
-        return product;
-      }
-    });
-    setSearchProducts(products);
-  }, [searchValue, searchProducts]);
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const [params, setParams] = useState({});
+  const [initialPage, setInitialPage] = useState(0);
+  const totalPages = useRef(1);
+  const cacheProducts = useSelector((state) => state.search.cache);
+  const [products, setProducts] = useState([]);
+  const [isEmptyResponse, setIsEmptyResponse] = useState(false);
+
+  const fetchProducts = useCallback(
+    async (page) => {
+      if (!page) return;
+      console.log("Fetching products", page);
+      const { data } = await productService.getMany({
+        ...params,
+        page: page,
+        sortBy: params.sortBy || "createdAt",
+        sort: params.sort || "d",
+      });
+
+      totalPages.current = Number(data.totalPages) || 1;
+      data.products.length === 0
+        ? setIsEmptyResponse(true)
+        : setIsEmptyResponse(false);
+
+      dispatch(
+        addQuery({
+          page,
+          query: params.query,
+          data: data.products,
+        })
+      );
+    },
+    [params]
+  );
+
+  // Store all request params in state
+  useEffect(() => {
+    const query = searchParams.get("query")?.trim().toLowerCase() || "";
+    const page = Number(searchParams.get("page")) || 0;
+    const category = searchParams.get("category")?.trim().toLowerCase() || "";
+    const company = searchParams.get("company")?.trim().toLowerCase() || "";
+    const sort = searchParams.get("sort")?.trim().toLowerCase() || "d";
+    const sortBy =
+      searchParams.get("sortBy")?.trim().toLowerCase() || "createdAt";
+
+    const newParams = {
+      query,
+      page,
+      category,
+      company,
+      sort,
+      sortBy,
+    };
+
+    setParams((prev) => ({ ...prev, ...newParams }));
+  }, [searchParams]);
 
   useEffect(() => {
-    if (!searchValue || allProducts.length === 0) {
-      navigate("/"); // TODO: navigate and send the state to scroll to the product section
-    } else filterProducts();
-  }, [searchValue]);
+    const query = params.query;
+    if (!cacheProducts?.[query]) {
+      setProducts([]);
+      setIsEmptyResponse(false);
+      return;
+    }
 
-  if (searchProducts.length === 0) {
-    return (
-      <div className="w-full text-center mt-10 font-bold text-2xl min-h-72">
-        No Products Found
-      </div>
-    );
-  }
+    const data = cacheProducts[params.query];
+    const lastPage = Number(Object.keys(data).at(-1));
+    setInitialPage(lastPage);
+    const products = Object.values(data).flat(1);
+    setProducts(products);
+  }, [cacheProducts, params]);
+
+  const [observerRef, page, isProductsLoading, productError, productRetry] =
+    useInfiniteScroll({
+      cb: fetchProducts,
+      initialPage,
+    });
+
+  useEffect(() => {
+    if (products.length === 0 && !isEmptyResponse) {
+      productRetry();
+    }
+  }, [params?.query, products]);
 
   return (
-    <div className="w-full flex flex-wrap items-start justify-evenly gap-3 gap-y-10 mt-10">
-      {searchProducts.map((product) => (
-        <ProductCard
-          key={product.$id}
-          name={product.name}
-          price={product.price}
-          image={product.image}
-          openProduct={() => navigate(`/product/${product.$id}`)}
-        />
-      ))}
+    <div className="w-full min-h-screen mt-10">
+      {products.length === 0 && isEmptyResponse && (
+        <div className="w-full max-w-xl mx-auto mt-16 p-6 text-center bg-white rounded-xl shadow-sm">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+            No Products Found
+          </h2>
+          <p className="text-gray-600 text-lg">
+            We couldn’t find anything for{" "}
+            <span className="text-black font-bold">
+              "{searchParams.get("query") || "your search"}"
+            </span>
+            .
+          </p>
+          <p className="text-gray-500 mt-2">
+            Try using different keywords or checking your spelling.
+          </p>
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="w-full flex flex-wrap items-start justify-evenly gap-3 gap-y-10 mb-5">
+          {products.map((product) => (
+            <ProductCard key={product?._id} product={product} />
+          ))}
+        </div>
+      )}
+      {productError ? (
+        <LoadingError error={productError} retry={productRetry} />
+      ) : (
+        (page !== totalPages.current || isProductsLoading) && (
+          <div className="w-full flex flex-wrap items-start justify-evenly gap-3 gap-y-10">
+            {Array.from({ length: 10 }).map((_, i) => (
+              <ProductCardShimmer key={i} ref={i === 0 ? observerRef : null} />
+            ))}
+          </div>
+        )
+      )}
     </div>
   );
 };
