363cb0cef1354370f2e8fc26e9e2bc6ddbb65f3c
Subrata Mondal <subratamondal@tutanota.com>
2025-05-15 10:35:52 +0530

    fix: update validation messages in Input component to use label instead of name for better clarity

--- frontend/src/components/formComp/Input.jsx
+++ frontend/src/components/formComp/Input.jsx
@@ -24,12 +24,12 @@ function Input({
 
   const getRules = (rules) => {
     if (rules.required) {
-      rules.required = `${name || "This Field"} is required`;
+      rules.required = `${label || "This Field"} is required`;
     }
     if (rules.minLength !== undefined && !isNaN(rules?.minLength)) {
       rules.minLength = {
         value: rules.minLength,
-        message: `${name || "This Field"} must be at least ${
+        message: `${label || "This Field"} must be at least ${
           rules.minLength
         } characters long`,
       };
@@ -37,7 +37,7 @@ function Input({
     if (rules.maxLength !== undefined && !isNaN(rules?.maxLength)) {
       rules.maxLength = {
         value: rules.maxLength,
-        message: `${name || "This Field"} must be at most ${
+        message: `${label || "This Field"} must be at most ${
           rules.maxLength
         } characters long`,
       };
@@ -45,7 +45,7 @@ function Input({
     if (rules.pattern !== undefined && rules.pattern?.message === undefined) {
       rules.pattern = {
         value: rules.pattern.value || rules.pattern,
-        message: rules.pattern.message || `${name || "This Field"} is invalid`,
+        message: rules.pattern.message || `${label || "This Field"} is invalid`,
       };
     }
 
