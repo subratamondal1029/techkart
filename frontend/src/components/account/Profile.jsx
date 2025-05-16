257d7aed577574984e39439de4458566ddfc6dd8
Subrata Mondal <subratamondal@tutanota.com>
2025-05-16 20:45:03 +0530

    fix: add password input handling and validation to profile update form for optimistic update

--- frontend/src/components/account/Profile.jsx
+++ frontend/src/components/account/Profile.jsx
@@ -1,14 +1,120 @@
-import React from "react";
-import { User2Icon, Mail, Briefcase } from "lucide-react";
+import { useState } from "react";
+
+import { Button, Input, UpdateForm } from "../";
 import AccountInfoCard from "./AccountInfoCard";
 
-const Profile = ({ userData }) => {
-  return (
-    <div className="flex flex-col w-3/4 space-y-5">
-      <h1 className="font-bold text-2xl">Account Information</h1>
-      <p className="text-gray-500">Update your account information here.</p>
+import {
+  Briefcase,
+  EyeIcon,
+  EyeOffIcon,
+  Mail,
+  PencilLine,
+  User2Icon,
+} from "lucide-react";
 
-      <div className="flex flex-wrap justify-start gap-3">
+import { useFormContext } from "react-hook-form";
+import { startTransition } from "react";
+import authService from "../../services/auth.service";
+import { useDispatch } from "react-redux";
+import { updateUserData } from "../../store/auth.slice";
+import showToast from "../../utils/showToast";
+
+const PasswordInput = () => {
+  const { watch } = useFormContext();
+  const [isPassword, setIsPassword] = useState(true);
+  const password = watch("password");
+
+  return (
+    <>
+      <div className="w-full relative flex items-center">
+        <Input
+          label="Password"
+          name="password"
+          type={isPassword ? "password" : "text"}
+          placeholder="Enter new Password"
+          rules={{
+            pattern: {
+              value:
+                /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
+              message:
+                "Password must be 8-20 characters, include 1 uppercase, 1 lowercase, 1 number, and 1 special character.",
+            },
+          }}
+        />
+        <div className="absolute bottom-2 right-2 cursor-pointer">
+          {isPassword ? (
+            <EyeIcon onClick={() => setIsPassword(!isPassword)} />
+          ) : (
+            <EyeOffIcon onClick={() => setIsPassword(!isPassword)} />
+          )}
+        </div>
+      </div>
+      <Input
+        label="Confirm Password"
+        name="confirmPassword"
+        type="password"
+        placeholder="Re-enter new Password"
+        rules={{
+          validate: (value) =>
+            !password || value === password || "Passwords do not match",
+        }}
+      />
+    </>
+  );
+};
+
+const Profile = ({ userData, setOptimisticUserData }) => {
+  const dispatch = useDispatch();
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitHandler = (data, methods) => {
+    delete data.confirmPassword;
+
+    const updatedData = Object.fromEntries(
+      Object.entries(data).filter(
+        ([key, value]) => userData[key] !== value && value !== ""
+      )
+    );
+
+    if (Object.keys(updatedData).length === 0) {
+      showToast("warning", "No changes detected");
+      return;
+    }
+
+    startTransition(async () => {
+      setIsLoading(true);
+      const { password, ...others } = updatedData;
+      setOptimisticUserData((prev) => ({
+        ...prev,
+        ...others,
+      }));
+
+      try {
+        const { data } = await authService.updateUser({
+          ...updatedData,
+        });
+
+        dispatch(updateUserData(data));
+        showToast("success", "Profile updated successfully");
+      } catch (error) {
+        showToast("error", error.message || "Something went wrong");
+      }
+
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <div className="flex flex-col w-full md:w-3/4 space-y-5">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-xl">Account Information</h1>
+        <Button onClick={() => setIsFormOpen(true)}>
+          <PencilLine size={20} className="mr-2" /> Update
+        </Button>
+      </div>
+
+      <div className="flex flex-wrap justify-center sm:justify-start gap-3 ">
         <AccountInfoCard
           label="Name"
           logo={
@@ -37,6 +143,26 @@ const Profile = ({ userData }) => {
           value={userData?.label || "user"}
         />
       </div>
+
+      {isFormOpen && (
+        <UpdateForm setIsOpen={setIsFormOpen} onSubmit={submitHandler}>
+          <Input
+            label="Name"
+            name="name"
+            type="text"
+            defaultValue={userData?.name}
+            rules={{ minLength: 3 }}
+          />
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            defaultValue={userData?.email}
+            rules={{ pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }}
+          />
+          <PasswordInput />
+        </UpdateForm>
+      )}
     </div>
   );
 };
